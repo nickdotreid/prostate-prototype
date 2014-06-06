992af94b65f73727da5761bcadc9e0bf61a45cca
Nick Reid <nick@Tonis-iMac.local>
2014-06-06 17:31:55 -0400

    got scroll magic to work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 var controller;
 $(document).ready(function(){
 	controller = new ScrollMagic();
+});
+$(document).ready(function(){
 	var scene = new ScrollScene({
-		triggerElement:"#scree2",
+		triggerElement:"#screen1",
+	}).setPin("#men").addTo(controller);
+
+	var scene = new ScrollScene({
+		triggerElement:"#turn-orange",
+	}).addTo(controller);
+	scene.on('start',function(event){
+		if(event.scrollDirection == 'FORWARD'){
+			$("#men").addClass('orange');
+		}else{
+			$("#men").removeClass('orange');
+		}
 	});
 });
