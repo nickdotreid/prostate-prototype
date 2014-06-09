96e75597e04650332bfce3b5cb0fac3ed57c5a91
Nick Reid <nickreid@nickreid.com>
2014-06-08 22:00:06 -0400

    messing about with things

--- js/main.js
+++ js/main.js
@@ -3,18 +3,22 @@ $(document).ready(function(){
 	controller = new ScrollMagic();
 });
 $(document).ready(function(){
-	var scene = new ScrollScene({
-		triggerElement:"#screen1",
-	}).setPin("#men").addTo(controller);
+	var thousand_men = $(".men.thousand");
+	$("#turn-orange").css("margin-bottom",thousand_men.height());
+	$(".screen").css("margin-bottom",$(window).height());
+});
 
+$(document).ready(function(){
 	var scene = new ScrollScene({
-		triggerElement:"#turn-orange",
-	}).addTo(controller);
+		triggerElement:"#men",
+		offset:$("#men").height()/2,
+	}).setPin("#men").addTo(controller);
 	scene.on('start',function(event){
 		if(event.scrollDirection == 'FORWARD'){
-			$("#men").addClass('orange');
+			$("#men").css("left",$(".men.thousand").offset().left);
 		}else{
-			$("#men").removeClass('orange');
+			$("#men").css("left",0);
 		}
+
 	});
 });
